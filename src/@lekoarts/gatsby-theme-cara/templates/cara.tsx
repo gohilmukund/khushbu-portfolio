ba77987bf4b7ea66923b8556d864c6cc65c0e738
Mukund Gohil <gohil.mukund@gmail.com>
2022-10-21 01:40:42 +0530

    Update cara.tsx

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -5,9 +5,24 @@ import Hero from "../components/hero"
 import Projects from "../components/projects"
 import About from "../components/about"
 import Contact from "../components/contact"
+import {Helmet} from "react-helmet";
+
+
+
 
 const Cara = () => (
   <Layout>
+    <Helmet>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BFZM5CFLR4"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BFZM5CFLR4');
+      </script>
+    </Helmet>
     <Parallax pages={7}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={3} />

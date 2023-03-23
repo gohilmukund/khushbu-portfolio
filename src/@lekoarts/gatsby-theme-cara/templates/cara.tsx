e1b8002cab96f87cb989b562d0d01a8bbf71c45d
mukund-gohil-atos <mukund.gohil@atos.net>
2023-03-23 23:11:28 +0530

    fixed

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -11,10 +11,10 @@ import Seo from "../components/seo"
 const Cara = () => (
   <Layout>
     <Parallax pages={10}>
-      <Hero offset={0} factor={2} />
-      <Projects offset={2} factor={5} />
-      <About offset={7} factor={2} />
-      <Contact offset={9} factor={1} />
+      <Hero offset={0} factor={1} />
+      <Projects offset={2} factor={3} />
+      <About offset={6} factor={2} />
+      <Contact offset={8} factor={2.2} />
     </Parallax>
   </Layout>
 )
